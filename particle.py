990d8be90bc838397b577ac74d92e0549bc3cb03
hudsonhadley <hudsonhadley@yahoo.com>
2026-01-08 17:24:45 -0500

    first commit

---
+++ particle.py
@@ -0,0 +1,9 @@
+class Particle:
+    def __init__(self, x, y, vx, vy, mass=5):
+        self.x = x
+        self.y = y
+        
+        self.vx = vx
+        self.vy = vy
+
+        self.mass = mass
---
+++ pole.py
@@ -0,0 +1,7 @@
+class Pole:
+    def __init__(self, x, y, is_push, strength=5):
+        self.x = x
+        self.y = y
+
+        self.is_push = is_push
+        self.strength = strength
