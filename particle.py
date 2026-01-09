acb4fe054ac833901a55825206c65dacea4b95f7
hudsonhadley <hudsonhadley@yahoo.com>
2026-01-09 14:56:52 -0500

    Using velocity magnitude to get color with hsv and then converting back to rgb

--- particle.py
+++ particle.py
@@ -17,8 +17,37 @@ class Particle :
         self.pos += self.vel * dt
 
     def get_color_from_velocity(self) -> tuple[int, int, int]:
-        magnitude: int = int(self.vel.magnitude())
-        if magnitude > 255:
-            magnitude = 255
 
-        return (magnitude, magnitude, magnitude)
+        # We'll use magnitude as the hue for hsb coloring and then convert to rgb
+        magnitude: int = int(self.vel.magnitude())
+        if magnitude > 360:
+            magnitude = 360
+
+        saturation: float = 1
+        brightness: float = 1
+
+        return hsv_to_rgb(magnitude, saturation, brightness)
+
+def hsv_to_rgb(h: float, s: float, v: float) -> tuple[int, int, int]:
+    h %= 360
+
+    chroma: float = v * s
+
+    X: float = chroma * (1 - abs((h / 60) % 2 - 1))
+    m: float = v - chroma
+
+    if h < 60:
+        r, g, b = (chroma, X, 0)
+    elif h < 120:
+        r, g, b = (X, chroma, 0)
+    elif h < 180:
+        r, g, b = (0, chroma, X)
+    elif h < 240:
+        r, g, b = (0, X, chroma)
+    elif h < 300:
+        r, g, b = (X, 0, chroma)
+    else:
+        r, g, b = (chroma, 0, X)
+
+    return (int((r+m)*255), int((g+m)*255), int((b+m)*255))
+        
