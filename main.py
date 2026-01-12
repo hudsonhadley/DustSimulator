59169acaa2aeb94653db3c5980e5c8766014ee81
hudsonhadley <hudsonhadley@yahoo.com>
2026-01-11 19:33:50 -0500

    Poles can move in circular movement

--- main.py
+++ main.py
@@ -38,13 +38,10 @@ def main():
     particle_count = 4000
 
     particles: list[Particle] = []
-    generate_circle(particles, particle_count//4, 500, 250, 40, vx=150)
-    generate_circle(particles, particle_count//4, 750, 500, 40, vy=150)
-    generate_circle(particles, particle_count//4, 500, 750, 40, vx=-150)
-    generate_circle(particles, particle_count//4, 250, 500, 40, vy=-150)
+    generate_circle(particles, particle_count, 500, 500, 10)
 
     poles: list[Pole] = [
-                         Pole(500, 500, 100)
+                         Pole(500, 500, 100, radius=300, speed=200)
                          ]
 
     push_pole_color: tuple[int, int, int] = (0, 0, 255)
@@ -67,6 +64,9 @@ def main():
 
             particle.apply_force(net_force, dt)
             particle.update(dt)
+
+        for pole in poles:
+            pole.move(dt)
 
         screen.fill("black")
 
--- pole.py
+++ pole.py
@@ -1,10 +1,32 @@
 from particle import Particle
 import pygame
+import math
 
 class Pole:
-    def __init__(self, x: float, y: float, strength: float):
+    def __init__(self, x: float, y: float, strength: float, radius: int|None=None, speed: int|None=None, direction: int|None=None):
+        """
+        Positive values pull particles while negative values push them.
+        Radius and speed should be assigned when wanting to put the pole on a circular path and the x,y given.
+        Speed and direction should be assigned when wanting to put the pole on a direct linear path from the x,y given.
+        """
         self.pos: pygame.Vector2 = pygame.Vector2(x, y)
         self.strength = strength # positive = pull, negative = push
+
+        # No path
+        if radius is None and speed is None and direction is None:
+            self.movement = None
+
+        # Circuit
+        elif radius is not None and speed is not None and direction is None:
+            self.movement = "Circuit"
+            self.center = pygame.Vector2(x, y)
+            self.pos += pygame.Vector2(radius, 0) # Put it at theta=0
+            self.radius = radius
+            self.speed = speed
+
+        # Line
+        elif radius is None and speed is not None and direction is not None:
+            self.movement = "Line"
 
     def force_on(self, particle: Particle) -> pygame.Vector2:
         direction: pygame.Vector2 = self.pos - particle.pos
@@ -18,3 +40,13 @@ class Pole:
         FORCE_SCALE = 2000000
         magnitude: float = FORCE_SCALE * self.strength / (distance * distance)
         return direction * magnitude
+    
+    def move(self, dt: float):
+        if self.movement is None:
+            return
+        elif self.movement == "Circuit":
+            theta = math.atan2(self.pos.y - self.center.y, self.pos.x - self.center.x)
+            theta += self.speed * dt / self.radius
+
+            self.pos.x = self.center.x + self.radius * math.cos(theta)
+            self.pos.y = self.center.y + self.radius * math.sin(theta)
